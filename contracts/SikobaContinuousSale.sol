9c7012c121b28cf50b6fbfa7ec5c67a3e2d00779
sikoba <atok@sikoba.com>
2017-05-16 23:46:17 +0200

    Create SikobaContinuousSale.sol
    no code yet ...

---
+++ contracts/SikobaContinuousSale.sol
@@ -0,0 +1,42 @@
+258 lines (232 sloc)  13 KB
+pragma solidity ^0.4.8;
+
+/**
+ * SIKOBA PRESALE CONTRACTS
+ *
+ * Version 0.1
+ *
+ * Authors Bok 'BokkyPooBah' Khoo, Alex Kampa
+ *
+ * MIT LICENSE Copyright 2017 Sikoba Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+
+
+/**
+ *
+ * Important information
+ *
+ * For details about the Sikoba continuous token sale, and in particular to find 
+ * out about risks and limitations, please visit:
+ *
+ * http://www.sikoba.com/www/presale/index.html
+ *
+ **/
+ 
+ 
