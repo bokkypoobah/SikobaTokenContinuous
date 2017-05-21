c41661aa19486f45fda0ebd0026bd13cb76951a9
alex-kampa <aleksander.kampa@gmail.com>
2017-05-21 22:04:41 +0200

    Soft limit, pausing sale ...
    Changes made using GitHub desktop. Probabyl buggy as hell.

--- contracts/SikobaContinuousSale.sol
+++ contracts/SikobaContinuousSale.sol
@@ -1,42 +1,16 @@
 pragma solidity ^0.4.8;
 
-/**
- * SIKOBA PRESALE CONTRACTS
- *
- * Version 0.1
- *
- * Authors Bok 'BokkyPooBah' Khoo, Alex Kampa
- *
- * MIT LICENSE Copyright 2017 Sikoba Ltd
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- **/
+// ----------------------------------------------------------------------------
+//
+// Important information
+//
+// For details about the Sikoba continuous token sale, and in particular to find 
+// out about risks and limitations, please visit:
+//
+// http://www.sikoba.com/www/presale/index.html
+//
+// ----------------------------------------------------------------------------
 
-
-/**
- *
- * Important information
- *
- * For details about the Sikoba continuous token sale, and in particular to find 
- * out about risks and limitations, please visit:
- *
- * http://www.sikoba.com/www/presale/index.html
- *
- **/
  
  contract Owned {
     address public owner;
@@ -65,7 +39,11 @@ pragma solidity ^0.4.8;
 }
 
 
+// ----------------------------------------------------------------------------
+//
 // ERC Token Standard #20 - https://github.com/ethereum/EIPs/issues/20
+//
+
 contract ERC20Token is Owned {
     uint256 _totalSupply = 0;
 
@@ -164,6 +142,9 @@ contract ERC20Token is Owned {
 }
 
 
+// ----------------------------------------------------------------------------
+//
+
 contract SikobaContinuousSale is ERC20Token {
 
     // ------------------------------------------------------------------------
@@ -176,80 +157,52 @@ contract SikobaContinuousSale is ERC20Token {
     // Thursday, 01-Jun-17 00:00:00 UTC
     uint256 public constant START_DATE = 1496275200;
 
-    // Sunday, 29-Oct-17 00:00:00 UTC
-    uint256 public constant END_DATE = 1509235200;
+    // Tuesday, 31-Oct-17 23:59:59 UTC
+    uint256 public constant END_DATE = 1509494399;
 
-    // starting number of skoo units per ETH (each skoo subdivisible to 10**18)
-    uint256 public constant START_SKOO_UNITS = 1650;
+    // number of SKOK units per ETH at beginning and end
+    uint256 public constant START_SKOK_UNITS = 1650;
+    uint256 public constant END_SKOK_UNITS = 1200;
 
-    // period between start and end dates: 150 days = 12,960,000 seconds
-    // decrease of skoo units per second = 34,722,222,222,222
-    // 12,960,000  34,722,222,222,222 = 449,999,999,999,997,120,000 ~ 450 10**18
-    // skoo units per ETH at the end = 1200,000,000,000,002,880,000 wei
-    uint256 public constant SKOO_DECREASE_PER_SECOND = 34722222222222;
     // maximum funding in US$
-    uint256 public constant SALE_MAXIMUM_FUNDING = 400000;
-
+    uint256 public constant MAX_USD_FUNDING = 400000;
+    bool public maxUsdFundingReached = false;
+    uint256 public usdPerHundredETH;
+    uint256 public totalUsdFunding;
+    uint256 public constant softEndDate = END_DATE;
+    
     address multisig;
     bool public mintingCompleted = false;
-
+    bool public fundingPaused = false;
     uint256 public deployedAt;
+
+    // ------------------------------------------------------------------------
+    // ???
+    // ------------------------------------------------------------------------
 
     function SikobaContinuousSale() {
         deployedAt = now;
     }
 
-
     // ------------------------------------------------------------------------
-    // Members buy tokens from this contract at this price
-    //
-    // This is a maximum price that the tokens should be bought at, as buyers
-    // can always buy tokens from this contract for this price
-    //
-    // Check out the BERP prices on https://cryptoderivatives.market/ to see
-    // if you can buy these tokens for less than this maximum price
+    // Owner settings
     // ------------------------------------------------------------------------
-    function buyPrice() constant returns (uint256) {
-        return buyPriceAt(now);
+    
+    function setUsdPerHundredETH (uint256 value) external onlyOwner {
+      usdPerHundredETH = value; // if coinmarketcap $131.14 then send 13114
+    }
+    
+    function pause () external onlyOwner {
+      fundingPaused = true;
     }
 
-    function buyPriceAt(uint256 at) constant returns (uint256) {
-        if (at < (deployedAt + 7 days)) {
-            return 10 * 10**14;
-        } else if (at < (deployedAt + 30 days)) {
-            return 11 * 10**14;
-        } else if (at < (deployedAt + 60 days)) {
-            return 12 * 10**15;
-        } else if (at < (deployedAt + 90 days)) {
-            return 13 * 10**15;
-        } else if (at < (deployedAt + 365 days)) {
-            return 15 * 10**16;
-        } else {
-            return 10**21;
-        }
+    function restart () external onlyOwner {
+      fundingPaused = false;
     }
 
-
-    // ------------------------------------------------------------------------
-    // Buy tokens from the contract
-    // ------------------------------------------------------------------------
-    function () payable {
-        buyTokens();
+    function setMintingCompleted() onlyOwner {
+        mintingCompleted = true;
     }
-
-    function buyTokens() payable {
-        if (msg.value > 0) {
-            uint tokens = msg.value * 1 ether / buyPrice();
-            _totalSupply += tokens;
-            balances[msg.sender] += tokens;
-            TokensBought(msg.sender, msg.value, this.balance, tokens,
-                 _totalSupply, buyPrice());
-            if (!multisig.send(msg.value)) throw;
-        }
-    }
-    event TokensBought(address indexed buyer, uint256 ethers, 
-        uint256 newEtherBalance, uint256 tokens, uint256 newTotalSupply, 
-        uint256 buyPrice);
 
 
     // ------------------------------------------------------------------------
@@ -263,12 +216,45 @@ contract SikobaContinuousSale is ERC20Token {
         Transfer(this, participant, tokens);
     }
 
+    // ------------------------------------------------------------------------
+    // Buy tokens from the contract
+    // ------------------------------------------------------------------------
 
-    // ------------------------------------------------------------------------
-    // No more mining to be done
-    // ------------------------------------------------------------------------
-    function setMintingCompleted() onlyOwner {
-        mintingCompleted = true;
+    function unitsPerEth() external returns (uint256) {
+      return START_SKOK_UNITS * 10**18 - (START_SKOK_UNITS - END_SKOK_UNITS) * 10**18 * (now - START_DATE) / (END_DATE - START_DATE)
     }
+
+    function () payable {
+        buyTokens();
+    }
+
+    function buyTokens() payable {
+        if (fundingPaused) throw;
+        if (now < START_DATE) throw;
+        if (now > END_DATE) throw;
+        if (now > softEndDate) throw;
+        if (maximumFundingReached) throw;
+        if (msg.value * 1 ether < 10**16) throw; // ?? not sure the * 1 ether is needed
+
+        uint256 units = unitsPerEth();
+        uint256 tokens = msg.value * 1 ether * units / 10**18;
+        _totalSupply += tokens;
+        totalFundingInUsd += msg.value * 1 ether * usdPerHundredETH / 10**20;
+        balances[msg.sender] += tokens;
+        
+        if (!maxUsdFundingReached && totalFundingInUsd > MAX_USD_FUNDING) {
+          softEndDate = now + 24*60*60;
+          maxUsdFundingReached = true;
+        }
+        
+        TokensBought(msg.sender, msg.value, this.balance, tokens, _totalSupply, units); // ??
+        if (!multisig.send(msg.value)) throw; // ??
+
+        event TokensBought(address indexed buyer, uint256 ethers, 
+          uint256 newEtherBalance, uint256 tokens, uint256 newTotalSupply, 
+          uint256 unitsPerEth);
+    }
+    
+
 }
  
